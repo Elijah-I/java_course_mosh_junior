6ef9377a190d789f63379ce0334e9c0791fc368a
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2026-05-19 16:01:19 +0300

    feat: Mortgage calculator

--- src/me/junior/Main.java
+++ src/me/junior/Main.java
@@ -1,7 +1,27 @@
 package me.junior;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
-    static public void main(String[] args) {
-        System.out.println("Hello world");
+    static void main(String[] args) {
+        final byte MONTH_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual interest rate: ");
+        float rate = scanner.nextFloat() / MONTH_IN_YEAR / PERCENT;
+
+        System.out.print("Period (Years): ");
+        int period = scanner.nextByte() * MONTH_IN_YEAR;
+
+        double ratedPeriod = Math.pow(1 + rate, period);
+        double total = (principal * (rate * ratedPeriod) / (ratedPeriod - 1));
+
+        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(total));
     }
 }

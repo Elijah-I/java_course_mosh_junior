4ed4b964dd36c108800726c7d4f4cf9ab8b62207
Ilya V. Ivanik (IIvanik) <iivanik@inno.tech>
2026-05-20 11:34:07 +0300

    refactor: Mortgage add verifications

--- src/me/junior/Main.java
+++ src/me/junior/Main.java
@@ -4,24 +4,83 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-    static void main(String[] args) {
-        final byte MONTH_IN_YEAR = 12;
-        final byte PERCENT = 100;
+    static final byte MONTH_IN_YEAR = 12;
+    static final byte PERCENT = 100;
 
+    static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = Main.getPrincipal(scanner);
+        float monthRate = Main.getMonthRate(scanner);
+        int period = Main.getPeriod(scanner);
 
-        System.out.print("Annual interest rate: ");
-        float rate = scanner.nextFloat() / MONTH_IN_YEAR / PERCENT;
+        String mortgage = Main.getMortgage(principal, period, monthRate);
 
-        System.out.print("Period (Years): ");
-        int period = scanner.nextByte() * MONTH_IN_YEAR;
+        System.out.println("Mortgage: " + mortgage);
+    }
 
-        double ratedPeriod = Math.pow(1 + rate, period);
-        double total = (principal * (rate * ratedPeriod) / (ratedPeriod - 1));
+    private static String getMortgage(int principal, int period, float monthRate) {
+        double ratedPeriod = Math.pow(1 + monthRate, period);
+        double total = (principal * (monthRate * ratedPeriod) / (ratedPeriod - 1));
 
-        System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(total));
+        return NumberFormat.getCurrencyInstance().format(total);
+    }
+
+    private static int getPrincipal(Scanner scanner) {
+        final int MIN_PRINCIPAL = 1_000;
+        final int MAX_PRINCIPAL = 1_000_000;
+
+        int principal = 0;
+
+        while (true) {
+            System.out.print("Principal ($1k - $1M): ");
+            principal = scanner.nextInt();
+            if (principal >= MIN_PRINCIPAL && principal <= MAX_PRINCIPAL) {
+                break;
+            }
+            System.out.println("Error: $1k - $1M");
+        }
+
+        return principal;
+    }
+
+    private static int getPeriod(Scanner scanner) {
+        final byte MIN_PERIOD = 1;
+        final byte MAX_PERIOD = 30;
+
+        int period = 0;
+
+        while (true) {
+            System.out.print("Period (Years): ");
+            period = scanner.nextByte() * Main.MONTH_IN_YEAR;
+
+            if (period >= MIN_PERIOD && period <= MAX_PERIOD) {
+                break;
+            }
+            System.out.println("Error: 1 - 30");
+        }
+
+        return period;
+    }
+
+    private static float getMonthRate(Scanner scanner) {
+        final byte MIN_RATE = 1;
+        final byte MAX_RATE = 30;
+
+        float rate;
+        float monthRate;
+
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            rate = scanner.nextFloat();
+
+            if (rate >= MIN_RATE && rate <= MAX_RATE) {
+                monthRate = rate / Main.MONTH_IN_YEAR / PERCENT;
+                break;
+            }
+            System.out.println("Error: 1 - 30");
+        }
+
+        return monthRate;
     }
 }
